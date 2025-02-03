872c6bae3ce74d16cb683e51872b25f42604c5c7
Harry Young <harry.young@hippodigital.co.uk>
2025-02-03 20:22:53 +0000

    Access secrets and pass to compose file

---
+++ src/e2e/CareLeavers.E2ETests/playwright-ci.config.ts
@@ -0,0 +1,52 @@
+import { defineConfig, devices } from '@playwright/test';
+import * as dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+export default defineConfig({
+    testDir: './src/tests',
+    testMatch: '**/*.spec.ts',
+    timeout: 30 * 1000, // 30 seconds
+    expect: {
+        timeout: 5000, // 5 seconds
+    },
+    fullyParallel: true,
+    retries: 1,
+    workers: 4,
+    reporter: [['list'], ['html', { open: 'never' }]],
+    use: {
+        trace: 'on-first-retry',
+        screenshot: 'only-on-failure',
+        video: 'retain-on-failure',
+        baseURL: 'http://localhost:7050/',
+    },
+    projects: [
+        {
+            name: 'Chromium',
+            use: { browserName: 'chromium' },
+        },
+        {
+            name: 'Firefox',
+            use: { browserName: 'firefox' },
+        },
+        {
+            name: 'WebKit',
+            use: { browserName: 'webkit' },
+        },
+        {
+            name: 'Mobile Chrome',
+            use: {
+                browserName: 'chromium',
+                ...devices['Pixel 8'],
+            },
+        },
+        {
+            name: 'Mobile Safari',
+            use: {
+                browserName: 'webkit',
+                ...devices['iPhone 15'],
+            },
+        },
+    ],
+});
