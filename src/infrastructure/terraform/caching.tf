a7ea67e49f6bfc367dc2d2d997bd3504eb043eac
Harry Young <harry.young@hippodigital.co.uk>
2025-02-06 14:03:39 +0000

    Add new variables and more environments

--- src/infrastructure/terraform/caching.tf
+++ src/infrastructure/terraform/caching.tf
@@ -1,15 +1,17 @@
 resource "azurerm_resource_group" "caching-rg" {
+  count    = lower(var.caching_type) == "redis" ? 1 : 0
   name     = "${local.service_prefix}-caching-rg"
   location = local.location
   tags     = local.common_tags
 }
 
 resource "azurerm_redis_cache" "redis-cache" {
+  count               = lower(var.caching_type) == "redis" ? 1 : 0
   capacity            = 2
   family              = "C"
   location            = local.location
   name                = "${local.service_prefix}-redis-cache"
-  resource_group_name = azurerm_resource_group.caching-rg.name
+  resource_group_name = azurerm_resource_group.caching-rg[0].name
   sku_name            = "Standard"
   minimum_tls_version = "1.2"
 
@@ -17,7 +19,8 @@ resource "azurerm_redis_cache" "redis-cache" {
 }
 
 resource "azurerm_key_vault_secret" "redis-cache-connection-string" {
+  count        = lower(var.caching_type) == "redis" ? 1 : 0
   key_vault_id = azurerm_key_vault.key-vault.id
   name         = "redis-cache-connection-string"
-  value        = azurerm_redis_cache.redis-cache.primary_connection_string
+  value        = azurerm_redis_cache.redis-cache[0].primary_connection_string
 }
