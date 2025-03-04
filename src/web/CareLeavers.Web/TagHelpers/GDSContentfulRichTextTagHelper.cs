4c8c6241d8dd67802156913ecbd65f17cd8498a1
Stuart Foulds-Duncan <stuart.duncan@hippodigital.co.uk>
2025-03-04 17:48:44 +0000

    Added contents block back in, plus larger text modifier into the rich content renderer (for the header)

--- src/web/CareLeavers.Web/TagHelpers/GDSContentfulRichTextTagHelper.cs
+++ src/web/CareLeavers.Web/TagHelpers/GDSContentfulRichTextTagHelper.cs
@@ -7,15 +7,36 @@ namespace CareLeavers.Web.TagHelpers;
 [HtmlTargetElement("gds-contentful-rich-text")]
 public class GDSContentfulRichTextTagHelper(HtmlRenderer renderer) : ContentfulRichTextTagHelper(renderer)
 {
+    public bool LargerText { get; set; } = false;
+    
     public override void Process(TagHelperContext context, TagHelperOutput output)
     {
         output.TagName = null;
         base.Process(context, output);
+        
+        if (LargerText)
+        {
+            var content = output.Content.GetContent();
+            content = content.Replace("govuk-body", "govuk-body-l");
+            output.Content = new DefaultTagHelperContent();
+            output.Content.SetHtmlContent(content);
+        }
+        
     }
     
     public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
     {
         output.TagName = null;
-        return base.ProcessAsync(context, output);
+        var result = base.ProcessAsync(context, output);
+        result.Wait();
+        if (LargerText)
+        {
+            var content = output.Content.GetContent();
+            content = content.Replace("govuk-body", "govuk-body-l");
+            output.Content = new DefaultTagHelperContent();
+            output.Content.SetHtmlContent(content);
+        }
+
+        return result;
     }
 }
