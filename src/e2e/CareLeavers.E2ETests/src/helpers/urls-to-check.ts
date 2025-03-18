9794fe875936b92d9c10d055a1c90d8e4bd894e4
varshakrishna-hippo <166136888+varshakrishna-hippo@users.noreply.github.com>
2025-03-18 17:21:21 +0000

    sorting the URL's

--- src/e2e/CareLeavers.E2ETests/src/helpers/urls-to-check.ts
+++ src/e2e/CareLeavers.E2ETests/src/helpers/urls-to-check.ts
@@ -9,41 +9,29 @@ export const commonPagesToTest = [
     '/en/education-and-training',
     '/en/health-and-wellbeing',
     '/en/unaccompanied-asylum-seeking-young-people',
-    /*
-   '/en/pathway-plan',
-   '/en/personal-adviser',
-   '/en/leaving-care-allowance',
-   '/en/higher-education-bursary',
-   '/en/local-offer-for-care-leavers',
-   
-   '/en/eligible-child',
-   '/en/relevant-child',
-   '/en/former-relevant-child',
-   '/en/person-qualifying-for-advice-and-assistance',
-   '/en/what-happens-when-you-leave-care',
-   '/en/care-terms-explained',
-    */
+    '/en/pathway-plan',
+    '/en/personal-adviser',
+    '/en/leaving-care-allowance',
+    '/en/higher-education-bursary',
+    '/en/local-offer-for-care-leavers',
+    '/en/eligible-child',
+    '/en/relevant-child',
+    '/en/former-relevant-child',
+    '/en/person-qualifying-for-advice-and-assistance',
+    '/en/what-happens-when-you-leave-care',
+    '/en/care-terms-explained',
     '/en/your-rights',
     '/en/leaving-care-guides',
     '/en/helplines',
 ];
 
 // List of helpline-related links to test-Only certain pages will have this link
-export const helplineLinksToTest = [
-    '/en/home',
-    '/en/all-support',
-];
+export const helplineLinksToTest = commonPagesToTest.filter(path => path !== '/helplines');
 
 // List of Pages that will have the share and print buttons
-export const shareAndPrintLinksToTest = [
-    '/en/money-and-benefits',
-    '/en/housing-and-accommodation',
-    '/en/work-and-employment',
-    '/en/education-and-training',
-    '/en/health-and-wellbeing',
-    '/en/unaccompanied-asylum-seeking-young-people',
-    '/en/helplines'
-];
+export const shareAndPrintLinksToTest = commonPagesToTest.filter(
+    path => !['/home', '/all-support', '/your-rights', '/leaving-care-guides'].includes(path)
+);
 
 // List of URLS and their expected Breadcrumbs
 export const breadcrumbTestData = [
@@ -54,5 +42,20 @@ export const breadcrumbTestData = [
     { urls: ['/en/eligible-child','/en/relevant-child','/en/former-relevant-child','/en/person-qualifying-for-advice-and-assistance'], expectedBreadcrumbs: ['Home','Your rights'] },
     { urls: ['/en/what-happens-when-you-leave-care','/en/care-terms-explained'], expectedBreadcrumbs: ['Home','Leaving care guides'] },
     // Add more urls cases as needed
+];
+
+// List of Pages that will have the Metadata
+export const metaDataLinksToTest = commonPagesToTest.filter(
+    path => !['/home', '/all-support', '/your-rights', '/leaving-care-guides','/helplines'].includes(path)
+);
+
+// List of support cards
+export const supportCards = [
+    { title: "Money and benefits", url: "/money-and-benefits" },
+    { title: "Housing and accommodation", url: "/housing-and-accommodation" },
+    { title: "Work and employment", url: "/work-and-employment" },
+    { title: "Education and training", url: "/education-and-training" },
+    { title: "Health and wellbeing", url: "/health-and-wellbeing" },
+    { title: "Unaccompanied asylum-seeking young people", url: "/unaccompanied-asylum-seeking-young-people" }
 ];
 
