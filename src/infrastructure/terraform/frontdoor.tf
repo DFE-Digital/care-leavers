0d07562c9f32f5358ffba281b90cf7249224e460
Harry Young <harry.young@hippodigital.co.uk>
2025-03-19 11:37:46 +0000

    Refactor and add thanks.txt

--- src/infrastructure/terraform/frontdoor.tf
+++ src/infrastructure/terraform/frontdoor.tf
@@ -84,32 +84,59 @@ resource "azurerm_cdn_frontdoor_security_policy" "frontdoor-web-security-policy"
   }
 }
 
-resource "azurerm_cdn_frontdoor_rule_set" "frontdoor-web-rule-set" {
+resource "azurerm_cdn_frontdoor_rule_set" "security_redirects" {
+  name                     = "${var.environment_prefix}SecurityRedirects"
   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.frontdoor-web-profile.id
-  name                     = "securityRuleSet"
 }
 
 resource "azurerm_cdn_frontdoor_rule" "security_txt_rule" {
-  name                      = "securitytxtredirect"
-  cdn_frontdoor_rule_set_id = azurerm_cdn_frontdoor_rule_set.frontdoor-web-rule-set.id
+  depends_on = [azurerm_cdn_frontdoor_origin_group.frontdoor-origin-group, azurerm_cdn_frontdoor_origin.frontdoor-web-origin]
+
+  name                      = "securityTxtRedirect"
+  cdn_frontdoor_rule_set_id = azurerm_cdn_frontdoor_rule_set.security_redirects.id
   order                     = 1
   behavior_on_match         = "Continue"
 
-  actions {
-
-    url_redirect_action {
-      redirect_type        = "PermanentRedirect"
-      redirect_protocol    = "Https"
-      destination_hostname = "vdp.security.education.gov.uk"
-      destination_path     = "/.well-known/security.txt"
+  conditions {
+    url_path_condition {
+      operator     = "BeginsWith"
+      match_values = [".well-known/security.txt", "security.txt"]
+      transforms   = ["Lowercase"]
     }
   }
 
+  actions {
+    url_redirect_action {
+      redirect_type        = "Found"
+      redirect_protocol    = "Https"
+      destination_hostname = "vdp.security.education.gov.uk"
+      destination_path     = "/security.txt"
+    }
+  }
+}
+
+resource "azurerm_cdn_frontdoor_rule" "thanks_txt_rule" {
+  depends_on = [azurerm_cdn_frontdoor_origin_group.frontdoor-origin-group, azurerm_cdn_frontdoor_origin.frontdoor-web-origin]
+
+  name                      = "thanksTxtRedirect"
+  cdn_frontdoor_rule_set_id = azurerm_cdn_frontdoor_rule_set.security_redirects.id
+  order                     = 1
+  behavior_on_match         = "Continue"
+
   conditions {
-    url_filename_condition {
+    url_path_condition {
       operator     = "BeginsWith"
-      match_values = ["security.txt", "/.well-known/security.txt"]
+      match_values = [".well-known/thanks.txt", "thanks.txt"]
       transforms   = ["Lowercase"]
+    }
+  }
+
+  actions {
+    url_redirect_action {
+      redirect_type        = "Found"
+      redirect_protocol    = "Https"
+      destination_hostname = "vdp.security.education.gov.uk"
+      destination_path     = "/thanks.txt"
     }
   }
 }
