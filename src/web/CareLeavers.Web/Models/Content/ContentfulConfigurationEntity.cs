e86e0dd9af6bfc6349106de7be62db74b4261d7a
Stuart Foulds-Duncan <stuart.duncan@hippodigital.co.uk>
2025-03-25 10:45:31 +0000

    - Add SEO descriptions to pages - Add default SEO image to config - Cleaned up models on page not found page

--- src/web/CareLeavers.Web/Models/Content/ContentfulConfigurationEntity.cs
+++ src/web/CareLeavers.Web/Models/Content/ContentfulConfigurationEntity.cs
@@ -31,4 +31,6 @@ public class ContentfulConfigurationEntity : ContentfulContent
     public bool TranslationEnabled { get; set; } = false;
     
     public List<string> ExcludeFromTranslation { get; set; } = [];
+    
+    public Asset? DefaultSeoImage { get; set; }
 }
