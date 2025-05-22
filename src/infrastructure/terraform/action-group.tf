029327e6fc36c35eaf39d05e164233c7fdce1e9d
Stuart Foulds-Duncan <stuart.duncan@hippodigital.co.uk>
2025-05-22 11:50:19 +0100

    Attempt to get some alerts working

---
+++ src/infrastructure/terraform/action-group.tf
@@ -0,0 +1,15 @@
+resource "azurerm_monitor_action_group" "service-support-action" {
+  name                = "Service support"
+  resource_group_name = azurerm_resource_group.web-rg.name
+  short_name          = "Support"
+  tags                = local.common_tags
+
+  dynamic "email_receiver" {
+    for_each = var.alerting[var.cip_environment].email_alerts_enabled ? ["true"] : []
+    content {
+      name                    = "send-to-support"
+      email_address           = var.support_alert_email
+      use_common_alert_schema = true
+    }
+  }
+}
