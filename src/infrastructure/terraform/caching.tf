c9cb8de9b1c13c74bed7d792f43f0c6926857b34
Olivia Hulston <oliviahulston@Mac.lan>
2026-01-12 15:03:04 +0000

    CTH-19 CTH-8 Adding a count flag on the resource

--- src/infrastructure/terraform/caching.tf
+++ src/infrastructure/terraform/caching.tf
@@ -58,5 +58,5 @@ resource "azurerm_key_vault_secret" "redis-enterprise-connection-string" {
   count        = lower(var.caching_type) == "redis" ? 1 : 0
   key_vault_id = azurerm_key_vault.key-vault.id
   name         = "redis-enterprise-connection-string"
-  value        = "${azurerm_managed_redis.redis-enterprise.hostname}:${azurerm_managed_redis.redis-enterprise.default_database[0].port},ssl=true,password=${azurerm_managed_redis.redis-enterprise.default_database[0].primary_access_key}"
+  value        = "${azurerm_managed_redis.redis-enterprise[0].hostname}:${azurerm_managed_redis.redis-enterprise[0].default_database[0].port},ssl=true,password=${azurerm_managed_redis.redis-enterprise[0].default_database[0].primary_access_key}"
 }
