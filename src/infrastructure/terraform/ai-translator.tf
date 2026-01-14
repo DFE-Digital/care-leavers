e96b511bb7922cab325d1ae7b80e61bd48ab013f
Olivia Hulston <oliviahulston@Olivias-MacBook-Pro.local>
2026-01-14 15:32:40 +0000

    CTH-19 CTH-17 AI translator disable local auth alterations

--- src/infrastructure/terraform/ai-translator.tf
+++ src/infrastructure/terraform/ai-translator.tf
@@ -11,4 +11,6 @@ resource "azurerm_cognitive_account" "ai-translator" {
   kind                = "TextTranslation" # Specifies the Translator service
   sku_name            = "S1"
   tags                = local.common_tags
+
+  local_auth_enabled = false
 }
