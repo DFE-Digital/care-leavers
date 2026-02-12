5d68c866ad4b81b7d9fe36b6d07be60dcd2818d2
Bob Lawson <armand.lawson@education.gov.uk>
2026-02-12 11:07:16 +0000

    CL-24 - Deploy minimal Front Door

--- src/infrastructure/terraform/frontdoor.tf
+++ src/infrastructure/terraform/frontdoor.tf
@@ -5,170 +5,170 @@ resource "azurerm_cdn_frontdoor_profile" "frontdoor-web-profile" {
   tags                = local.common_tags
 }
 
-resource "azurerm_cdn_frontdoor_origin_group" "frontdoor-origin-group" {
-  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.frontdoor-web-profile.id
-  name                     = "${local.service_prefix}-web-fd-origin-group"
-  session_affinity_enabled = false
+# resource "azurerm_cdn_frontdoor_origin_group" "frontdoor-origin-group" {
+#   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.frontdoor-web-profile.id
+#   name                     = "${local.service_prefix}-web-fd-origin-group"
+#   session_affinity_enabled = false
 
-  health_probe {
-    interval_in_seconds = 60
-    protocol            = "Https"
-    request_type        = "GET"
-    path                = "/health"
-  }
+#   health_probe {
+#     interval_in_seconds = 60
+#     protocol            = "Https"
+#     request_type        = "GET"
+#     path                = "/health"
+#   }
 
-  load_balancing {
-    sample_size                 = 4
-    successful_samples_required = 2
-  }
-}
+#   load_balancing {
+#     sample_size                 = 4
+#     successful_samples_required = 2
+#   }
+# }
 
-resource "azurerm_cdn_frontdoor_origin" "frontdoor-web-origin" {
-  cdn_frontdoor_origin_group_id  = azurerm_cdn_frontdoor_origin_group.frontdoor-origin-group.id
-  certificate_name_check_enabled = false
-  host_name                      = azurerm_linux_web_app.web-app-service.default_hostname
-  http_port                      = 80
-  https_port                     = 443
-  origin_host_header             = azurerm_linux_web_app.web-app-service.default_hostname
-  priority                       = 1
-  weight                         = 1
-  name                           = "${local.service_prefix}-web-fd-origin"
-  enabled                        = true
-}
+# resource "azurerm_cdn_frontdoor_origin" "frontdoor-web-origin" {
+#   cdn_frontdoor_origin_group_id  = azurerm_cdn_frontdoor_origin_group.frontdoor-origin-group.id
+#   certificate_name_check_enabled = false
+#   host_name                      = azurerm_linux_web_app.web-app-service.default_hostname
+#   http_port                      = 80
+#   https_port                     = 443
+#   origin_host_header             = azurerm_linux_web_app.web-app-service.default_hostname
+#   priority                       = 1
+#   weight                         = 1
+#   name                           = "${local.service_prefix}-web-fd-origin"
+#   enabled                        = true
+# }
 
-resource "azurerm_cdn_frontdoor_endpoint" "frontdoor-web-endpoint" {
-  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.frontdoor-web-profile.id
-  name                     = "${local.service_prefix}-web-fd-endpoint"
-  tags                     = local.common_tags
-}
+# resource "azurerm_cdn_frontdoor_endpoint" "frontdoor-web-endpoint" {
+#   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.frontdoor-web-profile.id
+#   name                     = "${local.service_prefix}-web-fd-endpoint"
+#   tags                     = local.common_tags
+# }
 
-resource "azurerm_cdn_frontdoor_route" "frontdoor-web-route" {
-  name                          = "${local.service_prefix}-web-fd-route"
-  cdn_frontdoor_endpoint_id     = azurerm_cdn_frontdoor_endpoint.frontdoor-web-endpoint.id
-  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.frontdoor-origin-group.id
-  cdn_frontdoor_origin_ids      = [azurerm_cdn_frontdoor_origin.frontdoor-web-origin.id]
-  cdn_frontdoor_rule_set_ids    = [azurerm_cdn_frontdoor_rule_set.security_redirects.id, azurerm_cdn_frontdoor_rule_set.security_headers.id]
-  enabled                       = true
+# resource "azurerm_cdn_frontdoor_route" "frontdoor-web-route" {
+#   name                          = "${local.service_prefix}-web-fd-route"
+#   cdn_frontdoor_endpoint_id     = azurerm_cdn_frontdoor_endpoint.frontdoor-web-endpoint.id
+#   cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.frontdoor-origin-group.id
+#   cdn_frontdoor_origin_ids      = [azurerm_cdn_frontdoor_origin.frontdoor-web-origin.id]
+#   cdn_frontdoor_rule_set_ids    = [azurerm_cdn_frontdoor_rule_set.security_redirects.id, azurerm_cdn_frontdoor_rule_set.security_headers.id]
+#   enabled                       = true
 
-  forwarding_protocol    = "MatchRequest"
-  https_redirect_enabled = true
-  patterns_to_match      = ["/*"]
-  supported_protocols    = ["Http", "Https"]
+#   forwarding_protocol    = "MatchRequest"
+#   https_redirect_enabled = true
+#   patterns_to_match      = ["/*"]
+#   supported_protocols    = ["Http", "Https"]
 
-  cdn_frontdoor_custom_domain_ids = var.custom_domain != "" ? [azurerm_cdn_frontdoor_custom_domain.fd-custom-domain[0].id] : null
-  link_to_default_domain          = true
-}
+#   cdn_frontdoor_custom_domain_ids = var.custom_domain != "" ? [azurerm_cdn_frontdoor_custom_domain.fd-custom-domain[0].id] : null
+#   link_to_default_domain          = true
+# }
 
-resource "azurerm_cdn_frontdoor_security_policy" "frontdoor-web-security-policy" {
-  name                     = "${local.service_prefix}-web-fd-security-policy"
-  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.frontdoor-web-profile.id
+# resource "azurerm_cdn_frontdoor_security_policy" "frontdoor-web-security-policy" {
+#   name                     = "${local.service_prefix}-web-fd-security-policy"
+#   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.frontdoor-web-profile.id
 
-  security_policies {
-    firewall {
-      cdn_frontdoor_firewall_policy_id = azurerm_cdn_frontdoor_firewall_policy.web_firewall_policy.id
+#   security_policies {
+#     firewall {
+#       cdn_frontdoor_firewall_policy_id = azurerm_cdn_frontdoor_firewall_policy.web_firewall_policy.id
 
-      association {
-        domain {
-          cdn_frontdoor_domain_id = azurerm_cdn_frontdoor_endpoint.frontdoor-web-endpoint.id
-        }
+#       association {
+#         domain {
+#           cdn_frontdoor_domain_id = azurerm_cdn_frontdoor_endpoint.frontdoor-web-endpoint.id
+#         }
 
-        dynamic "domain" {
-          for_each = var.custom_domain != "" ? ["apply"] : []
-          content {
-            cdn_frontdoor_domain_id = azurerm_cdn_frontdoor_custom_domain.fd-custom-domain[0].id
-          }
-        }
+#         dynamic "domain" {
+#           for_each = var.custom_domain != "" ? ["apply"] : []
+#           content {
+#             cdn_frontdoor_domain_id = azurerm_cdn_frontdoor_custom_domain.fd-custom-domain[0].id
+#           }
+#         }
 
-        patterns_to_match = ["/*"]
-      }
-    }
-  }
-}
+#         patterns_to_match = ["/*"]
+#       }
+#     }
+#   }
+# }
 
-resource "azurerm_cdn_frontdoor_rule_set" "security_headers" {
-  name                     = "${var.environment_prefix}SecurityHeaders"
-  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.frontdoor-web-profile.id
-}
+# resource "azurerm_cdn_frontdoor_rule_set" "security_headers" {
+#   name                     = "${var.environment_prefix}SecurityHeaders"
+#   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.frontdoor-web-profile.id
+# }
 
-resource "azurerm_cdn_frontdoor_rule" "security_headers_rule" {
-  depends_on = [azurerm_cdn_frontdoor_origin_group.frontdoor-origin-group, azurerm_cdn_frontdoor_origin.frontdoor-web-origin]
+# resource "azurerm_cdn_frontdoor_rule" "security_headers_rule" {
+#   depends_on = [azurerm_cdn_frontdoor_origin_group.frontdoor-origin-group, azurerm_cdn_frontdoor_origin.frontdoor-web-origin]
 
-  name                      = "securityHeaders"
-  cdn_frontdoor_rule_set_id = azurerm_cdn_frontdoor_rule_set.security_headers.id
-  order                     = 0
-  behavior_on_match         = "Continue"
+#   name                      = "securityHeaders"
+#   cdn_frontdoor_rule_set_id = azurerm_cdn_frontdoor_rule_set.security_headers.id
+#   order                     = 0
+#   behavior_on_match         = "Continue"
 
-  actions {
-    response_header_action {
-      header_action = "Overwrite"
-      header_name   = "Strict-Transport-Security"
-      value         = "max-age=31536000; includeSubDomains; preload"
-    }
-    response_header_action {
-      header_action = "Overwrite"
-      header_name   = "X-Content-Type-Options"
-      value         = "nosniff"
-    }
-  }
-}
+#   actions {
+#     response_header_action {
+#       header_action = "Overwrite"
+#       header_name   = "Strict-Transport-Security"
+#       value         = "max-age=31536000; includeSubDomains; preload"
+#     }
+#     response_header_action {
+#       header_action = "Overwrite"
+#       header_name   = "X-Content-Type-Options"
+#       value         = "nosniff"
+#     }
+#   }
+# }
 
 
-resource "azurerm_cdn_frontdoor_rule_set" "security_redirects" {
-  name                     = "${var.environment_prefix}SecurityRedirects"
-  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.frontdoor-web-profile.id
-}
+# resource "azurerm_cdn_frontdoor_rule_set" "security_redirects" {
+#   name                     = "${var.environment_prefix}SecurityRedirects"
+#   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.frontdoor-web-profile.id
+# }
 
-resource "azurerm_cdn_frontdoor_rule" "security_txt_rule" {
-  depends_on = [azurerm_cdn_frontdoor_origin_group.frontdoor-origin-group, azurerm_cdn_frontdoor_origin.frontdoor-web-origin]
+# resource "azurerm_cdn_frontdoor_rule" "security_txt_rule" {
+#   depends_on = [azurerm_cdn_frontdoor_origin_group.frontdoor-origin-group, azurerm_cdn_frontdoor_origin.frontdoor-web-origin]
 
-  name                      = "securityTxtRedirect"
-  cdn_frontdoor_rule_set_id = azurerm_cdn_frontdoor_rule_set.security_redirects.id
-  order                     = 0
-  behavior_on_match         = "Continue"
+#   name                      = "securityTxtRedirect"
+#   cdn_frontdoor_rule_set_id = azurerm_cdn_frontdoor_rule_set.security_redirects.id
+#   order                     = 0
+#   behavior_on_match         = "Continue"
 
-  conditions {
-    url_path_condition {
-      operator     = "BeginsWith"
-      match_values = [".well-known/security.txt", "security.txt"]
-      transforms   = ["Lowercase"]
-    }
-  }
+#   conditions {
+#     url_path_condition {
+#       operator     = "BeginsWith"
+#       match_values = [".well-known/security.txt", "security.txt"]
+#       transforms   = ["Lowercase"]
+#     }
+#   }
 
-  actions {
-    url_redirect_action {
-      redirect_type        = "PermanentRedirect"
-      redirect_protocol    = "Https"
-      destination_hostname = "vdp.security.education.gov.uk"
-      destination_path     = "/security.txt"
-    }
-  }
-}
+#   actions {
+#     url_redirect_action {
+#       redirect_type        = "PermanentRedirect"
+#       redirect_protocol    = "Https"
+#       destination_hostname = "vdp.security.education.gov.uk"
+#       destination_path     = "/security.txt"
+#     }
+#   }
+# }
 
-resource "azurerm_cdn_frontdoor_rule" "thanks_txt_rule" {
-  depends_on = [azurerm_cdn_frontdoor_origin_group.frontdoor-origin-group, azurerm_cdn_frontdoor_origin.frontdoor-web-origin]
+# resource "azurerm_cdn_frontdoor_rule" "thanks_txt_rule" {
+#   depends_on = [azurerm_cdn_frontdoor_origin_group.frontdoor-origin-group, azurerm_cdn_frontdoor_origin.frontdoor-web-origin]
 
-  name                      = "thanksTxtRedirect"
-  cdn_frontdoor_rule_set_id = azurerm_cdn_frontdoor_rule_set.security_redirects.id
-  order                     = 1
-  behavior_on_match         = "Continue"
+#   name                      = "thanksTxtRedirect"
+#   cdn_frontdoor_rule_set_id = azurerm_cdn_frontdoor_rule_set.security_redirects.id
+#   order                     = 1
+#   behavior_on_match         = "Continue"
 
-  conditions {
-    url_path_condition {
-      operator     = "BeginsWith"
-      match_values = [".well-known/thanks.txt", "thanks.txt"]
-      transforms   = ["Lowercase"]
-    }
-  }
+#   conditions {
+#     url_path_condition {
+#       operator     = "BeginsWith"
+#       match_values = [".well-known/thanks.txt", "thanks.txt"]
+#       transforms   = ["Lowercase"]
+#     }
+#   }
 
-  actions {
-    url_redirect_action {
-      redirect_type        = "PermanentRedirect"
-      redirect_protocol    = "Https"
-      destination_hostname = "vdp.security.education.gov.uk"
-      destination_path     = "/thanks.txt"
-    }
-  }
-}
+#   actions {
+#     url_redirect_action {
+#       redirect_type        = "PermanentRedirect"
+#       redirect_protocol    = "Https"
+#       destination_hostname = "vdp.security.education.gov.uk"
+#       destination_path     = "/thanks.txt"
+#     }
+#   }
+# }
 
 resource "azurerm_cdn_frontdoor_custom_domain" "fd-custom-domain" {
   count                    = var.custom_domain != "" ? 1 : 0
