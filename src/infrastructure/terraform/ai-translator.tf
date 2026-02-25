cdd54c8db762b2aba720b88eb762be15f576269f
oliviahulston8 <olivia.hulston@hippodigital.co.uk>
2026-02-25 10:56:21 +0000

    CL-24 Ensuring ai translator resource is not publically acessible

--- src/infrastructure/terraform/ai-translator.tf
+++ src/infrastructure/terraform/ai-translator.tf
@@ -5,10 +5,11 @@ resource "azurerm_resource_group" "translator-rg" {
 }
 
 resource "azurerm_cognitive_account" "ai-translator" {
-  name                = "${local.service_prefix}-ai-translation"
-  location            = azurerm_resource_group.translator-rg.location
-  resource_group_name = azurerm_resource_group.translator-rg.name
-  kind                = "TextTranslation" # Specifies the Translator service
-  sku_name            = "S1"
-  tags                = local.common_tags
+  name                          = "${local.service_prefix}-ai-translation"
+  location                      = azurerm_resource_group.translator-rg.location
+  resource_group_name           = azurerm_resource_group.translator-rg.name
+  kind                          = "TextTranslation" # Specifies the Translator service
+  sku_name                      = "S1"
+  tags                          = local.common_tags
+  public_network_access_enabled = false # Disable public endpoint for enhanced security
 }
