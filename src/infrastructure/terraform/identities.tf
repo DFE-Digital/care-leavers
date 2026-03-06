94e08619867d98d9f39e1522a0ba75a773c11a21
oliviahulston8 <olivia.hulston@hippodigital.co.uk>
2026-03-06 15:00:16 +0000

    feat(CL-48): Testing rbac changes for keyvault

---
+++ src/infrastructure/terraform/identities.tf
@@ -0,0 +1,7 @@
+resource "azurerm_user_assigned_identity" "cl-identity-reader" {
+  name                = "${local.service_prefix}mid-uks-cl-r"
+  location            = local.location
+  resource_group_name = azurerm_resource_group.core-rg.name
+
+  tags = local.common_tags
+}
