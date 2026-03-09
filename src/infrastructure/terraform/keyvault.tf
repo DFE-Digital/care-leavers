8925e9356b2abe10f5e56f6e1c414cfe40ea5c1e
oliviahulston8 <olivia.hulston@hippodigital.co.uk>
2026-03-09 10:05:21 +0000

    test(CL-48) Renaming resources

--- src/infrastructure/terraform/keyvault.tf
+++ src/infrastructure/terraform/keyvault.tf
@@ -84,7 +84,7 @@ resource "azurerm_role_assignment" "kv_admin_sp" {
 
 # Secrets
 
-resource "azurerm_key_vault_secret" "contentful-delivery-api-key" {
+resource "azurerm_key_vault_secret" "contentful-delivery-api-key-keyvault" {
   key_vault_id = azurerm_key_vault.kv.id
   name         = "contentful-delivery-api-key"
   value        = var.contentful_delivery_api_key
@@ -96,7 +96,7 @@ resource "azurerm_key_vault_secret" "contentful-delivery-api-key" {
   ]
 }
 
-resource "azurerm_key_vault_secret" "contentful-preview-api-key" {
+resource "azurerm_key_vault_secret" "contentful-preview-api-key-keyvault" {
   key_vault_id = azurerm_key_vault.kv.id
   name         = "contentful-preview-api-key"
   value        = var.contentful_preview_api_key
@@ -108,7 +108,7 @@ resource "azurerm_key_vault_secret" "contentful-preview-api-key" {
   ]
 }
 
-resource "azurerm_key_vault_secret" "contentful-management-api-key" {
+resource "azurerm_key_vault_secret" "contentful-management-api-key-keyvault" {
   key_vault_id = azurerm_key_vault.kv.id
   name         = "contentful-management-api-key"
   value        = var.contentful_management_api_key
@@ -120,7 +120,7 @@ resource "azurerm_key_vault_secret" "contentful-management-api-key" {
   ]
 }
 
-resource "azurerm_key_vault_secret" "contentful-space-id" {
+resource "azurerm_key_vault_secret" "contentful-space-id-keyvault" {
   key_vault_id = azurerm_key_vault.kv.id
   name         = "contentful-space-id"
   value        = var.contentful_space_id
@@ -132,7 +132,7 @@ resource "azurerm_key_vault_secret" "contentful-space-id" {
   ]
 }
 
-resource "azurerm_key_vault_secret" "application-insights-connection-string" {
+resource "azurerm_key_vault_secret" "application-insights-connection-string-keyvault" {
   key_vault_id = azurerm_key_vault.kv.id
   name         = "application-insights-connection-string"
   value        = azurerm_application_insights.application-insights.connection_string
@@ -144,7 +144,7 @@ resource "azurerm_key_vault_secret" "application-insights-connection-string" {
   ]
 }
 
-resource "azurerm_key_vault_secret" "pdf-generation-api-key" {
+resource "azurerm_key_vault_secret" "pdf-generation-api-key-keyvault" {
   key_vault_id = azurerm_key_vault.kv.id
   name         = "pdf-generation-api-key"
   value        = var.pdf_generation_api_key
@@ -156,7 +156,7 @@ resource "azurerm_key_vault_secret" "pdf-generation-api-key" {
   ]
 }
 
-resource "azurerm_key_vault_secret" "azure-translation-access-key" {
+resource "azurerm_key_vault_secret" "azure-translation-access-key-keyvault" {
   key_vault_id = azurerm_key_vault.kv.id
   name         = "azure-translation-access-key"
   value        = azurerm_cognitive_account.ai-translator.primary_access_key
@@ -170,7 +170,7 @@ resource "azurerm_key_vault_secret" "azure-translation-access-key" {
 
 # key-vault secrets
 
-resource "azurerm_key_vault_secret" "contentful-delivery-api-key" {
+resource "azurerm_key_vault_secret" "contentful-delivery-api-key-keyvault" {
   key_vault_id = azurerm_key_vault.key-vault.id
   name         = "contentful-delivery-api-key"
   value        = var.contentful_delivery_api_key
@@ -178,7 +178,7 @@ resource "azurerm_key_vault_secret" "contentful-delivery-api-key" {
   depends_on = [azurerm_key_vault_access_policy.github-kv-access]
 }
 
-resource "azurerm_key_vault_secret" "contentful-preview-api-key" {
+resource "azurerm_key_vault_secret" "contentful-preview-api-key-keyvault" {
   key_vault_id = azurerm_key_vault.key-vault.id
   name         = "contentful-preview-api-key"
   value        = var.contentful_preview_api_key
@@ -186,7 +186,7 @@ resource "azurerm_key_vault_secret" "contentful-preview-api-key" {
   depends_on = [azurerm_key_vault_access_policy.github-kv-access]
 }
 
-resource "azurerm_key_vault_secret" "contentful-management-api-key" {
+resource "azurerm_key_vault_secret" "contentful-management-api-key-keyvault" {
   key_vault_id = azurerm_key_vault.key-vault.id
   name         = "contentful-management-api-key"
   value        = var.contentful_management_api_key
@@ -194,7 +194,7 @@ resource "azurerm_key_vault_secret" "contentful-management-api-key" {
   depends_on = [azurerm_key_vault_access_policy.github-kv-access]
 }
 
-resource "azurerm_key_vault_secret" "contentful-space-id" {
+resource "azurerm_key_vault_secret" "contentful-space-id-keyvault" {
   key_vault_id = azurerm_key_vault.key-vault.id
   name         = "contentful-space-id"
   value        = var.contentful_space_id
@@ -202,7 +202,7 @@ resource "azurerm_key_vault_secret" "contentful-space-id" {
   depends_on = [azurerm_key_vault_access_policy.github-kv-access]
 }
 
-resource "azurerm_key_vault_secret" "application-insights-connection-string" {
+resource "azurerm_key_vault_secret" "application-insights-connection-string-keyvault" {
   key_vault_id = azurerm_key_vault.key-vault.id
   name         = "application-insights-connection-string"
   value        = azurerm_application_insights.application-insights.connection_string
@@ -210,7 +210,7 @@ resource "azurerm_key_vault_secret" "application-insights-connection-string" {
   depends_on = [azurerm_key_vault_access_policy.github-kv-access]
 }
 
-resource "azurerm_key_vault_secret" "pdf-generation-api-key" {
+resource "azurerm_key_vault_secret" "pdf-generation-api-key-keyvault" {
   key_vault_id = azurerm_key_vault.key-vault.id
   name         = "pdf-generation-api-key"
   value        = var.pdf_generation_api_key
@@ -218,7 +218,7 @@ resource "azurerm_key_vault_secret" "pdf-generation-api-key" {
   depends_on = [azurerm_key_vault_access_policy.github-kv-access]
 }
 
-resource "azurerm_key_vault_secret" "azure-translation-access-key" {
+resource "azurerm_key_vault_secret" "azure-translation-access-key-keyvault" {
   key_vault_id = azurerm_key_vault.key-vault.id
   name         = "azure-translation-access-key"
   value        = azurerm_cognitive_account.ai-translator.primary_access_key
