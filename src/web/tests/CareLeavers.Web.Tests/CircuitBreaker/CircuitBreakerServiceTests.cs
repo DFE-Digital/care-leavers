117a1f2a99e3bd959152506ff92a5eecfd604194
Zac-Digital <119302767+Zac-Digital@users.noreply.github.com>
2026-05-08 10:27:22 +0100

    test: update circuit breaker service tests

--- src/web/tests/CareLeavers.Web.Tests/CircuitBreaker/CircuitBreakerServiceTests.cs
+++ src/web/tests/CareLeavers.Web.Tests/CircuitBreaker/CircuitBreakerServiceTests.cs
@@ -21,18 +21,43 @@ public class CircuitBreakerServiceTests
         _httpContext = new DefaultHttpContext();
         _httpContext.Session = new MockSession();
         _httpContextAccessor.HttpContext = _httpContext;
-        
-        _circuitBreakerOptions = Options.Create(new CircuitBreakerOptions 
-        { 
+
+        _circuitBreakerOptions = Options.Create(new CircuitBreakerOptions
+        {
             AzureTranslationLimit = 2,
             PdfGeneratorLimit = 2
         });
-        
+
         _circuitBreakerService = new CircuitBreakerService(_httpContextAccessor, _circuitBreakerOptions);
     }
 
     [Test]
-    public void ShouldBreakCircuitAzureTranslation_WhenLanguageIsEnglish_ReturnsFalse()
+    public void ShouldBreakCircuit_When_HttpContextIsNull_Throws_ArgumentNullException()
+    {
+        _httpContextAccessor.HttpContext = null;
+
+        Assert.Throws<ArgumentNullException>(() =>
+            _circuitBreakerService.ShouldBreakCircuit(CircuitBreakerType.AzureTranslation));
+    }
+
+    [Test]
+    public void ShouldBreakCircuit_When_CircuitBreakerType_IsNull_Throws_ArgumentOutOfRangeException()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => _circuitBreakerService.ShouldBreakCircuit((CircuitBreakerType)2));
+    }
+
+    [Test]
+    public void ShouldBreakCircuit_AzureTranslation_When_LanguageCode_IsNull_Returns_False()
+    {
+        _httpContext.Request.RouteValues["languageCode"] = null;
+        
+        bool result = _circuitBreakerService.ShouldBreakCircuit(CircuitBreakerType.AzureTranslation);
+        
+        Assert.That(result, Is.False);
+    }
+
+    [Test]
+    public void ShouldBreakCircuit_AzureTranslation_When_LanguageCode_IsEnglish_Returns_False()
     {
         _httpContext.Request.RouteValues["languageCode"] = "en";
         
@@ -42,24 +67,36 @@ public class CircuitBreakerServiceTests
     }
 
     [Test]
-    public void ShouldBreakCircuitAzureTranslation_WhenLanguageIsNotEnglishAndLimitNotReached_ReturnsFalseAndAddsToSession()
+    public void ShouldBreakCircuit_AzureTranslation_When_JsonIsNull_Returns_False()
     {
         _httpContext.Request.RouteValues["languageCode"] = "sv";
         
         bool result = _circuitBreakerService.ShouldBreakCircuit(CircuitBreakerType.AzureTranslation);
         
-        Assert.That(result, Is.False);
-        
-        string? json = _httpContext.Session.GetString(CircuitBreakerOptions.AzureTranslationKey);
-        Assert.That(json, Is.Not.Null);
-        List<string>? translatedLanguages = JsonSerializer.Deserialize<List<string>>(json);
-        Assert.That(translatedLanguages, Is.Not.Null);
-        Assert.That(translatedLanguages, Is.Not.Empty);
-        Assert.That(translatedLanguages, Contains.Item("sv"));
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(result, Is.False);
+            Assert.That(_httpContext.Session.GetString(CircuitBreakerOptions.AzureTranslationKey), Is.EqualTo(JsonSerializer.Serialize(new List<string> { "sv" })));
+        }
     }
 
     [Test]
-    public void ShouldBreakCircuitAzureTranslation_WhenLanguagePreviouslyTranslated_ReturnsFalse()
+    public void ShouldBreakCircuit_AzureTranslation_When_JsonValueIsNull_Returns_False()
+    {
+        _httpContext.Request.RouteValues["languageCode"] = "sv";
+        _httpContext.Session.SetString(CircuitBreakerOptions.AzureTranslationKey, "null");
+        
+        bool result = _circuitBreakerService.ShouldBreakCircuit(CircuitBreakerType.AzureTranslation);
+        
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(result, Is.False);
+            Assert.That(_httpContext.Session.GetString(CircuitBreakerOptions.AzureTranslationKey), Is.EqualTo(JsonSerializer.Serialize(new List<string> { "sv" })));
+        }
+    }
+
+    [Test]
+    public void ShouldBreakCircuit_AzureTranslation_When_LanguagePreviouslyTranslated_Returns_False()
     {
         _httpContext.Request.RouteValues["languageCode"] = "sv";
         _httpContext.Session.SetString(CircuitBreakerOptions.AzureTranslationKey, JsonSerializer.Serialize(new List<string> { "sv" }));
@@ -68,12 +105,23 @@ public class CircuitBreakerServiceTests
         
         Assert.That(result, Is.False);
     }
+    
+    [Test]
+    public void ShouldBreakCircuit_AzureTranslation_When_AtLimit_But_LanguagePreviouslyTranslated_Returns_False()
+    {
+        _httpContext.Request.RouteValues["languageCode"] = "sv";
+        _httpContext.Session.SetString(CircuitBreakerOptions.AzureTranslationKey, JsonSerializer.Serialize(new List<string> { "sv", "fr" }));
+        
+        bool result = _circuitBreakerService.ShouldBreakCircuit(CircuitBreakerType.AzureTranslation);
+        
+        Assert.That(result, Is.False);
+    }
 
     [Test]
-    public void ShouldBreakCircuitAzureTranslation_WhenLimitReached_ReturnsTrue()
+    public void ShouldBreakCircuit_AzureTranslation_When_LimitIsReached_Returns_True()
     {
-        _httpContext.Request.RouteValues["languageCode"] = "es";
-        _httpContext.Session.SetString(CircuitBreakerOptions.AzureTranslationKey, JsonSerializer.Serialize(new List<string> { "sv", "fr" }));
+        _httpContext.Request.RouteValues["languageCode"] = "sv";
+        _httpContext.Session.SetString(CircuitBreakerOptions.AzureTranslationKey, JsonSerializer.Serialize(new List<string> { "es", "fr" }));
         
         bool result = _circuitBreakerService.ShouldBreakCircuit(CircuitBreakerType.AzureTranslation);
         
@@ -81,7 +129,22 @@ public class CircuitBreakerServiceTests
     }
 
     [Test]
-    public void ShouldBreakCircuitPdfGenerator_WhenUsageBelowLimit_ReturnsFalseAndIncrementsCount()
+    public void ShouldBreakCircuit_AzureTranslation_When_LimitIsNotReached_Returns_False_And_UpdatesSession()
+    {
+        _httpContext.Request.RouteValues["languageCode"] = "sv";
+        _httpContext.Session.SetString(CircuitBreakerOptions.AzureTranslationKey, JsonSerializer.Serialize(new List<string> { "es" }));
+        
+        bool result = _circuitBreakerService.ShouldBreakCircuit(CircuitBreakerType.AzureTranslation);
+        
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(result, Is.False);
+            Assert.That(_httpContext.Session.GetString(CircuitBreakerOptions.AzureTranslationKey), Is.EqualTo(JsonSerializer.Serialize(new List<string> { "es", "sv" })));
+        }
+    }
+
+    [Test]
+    public void ShouldBreakCircuit_PdfGenerator_When_FirstUsage_Returns_False_And_UpdatesSession()
     {
         bool result = _circuitBreakerService.ShouldBreakCircuit(CircuitBreakerType.PdfGenerator);
         
@@ -93,19 +156,23 @@ public class CircuitBreakerServiceTests
     }
 
     [Test]
-    public void ShouldBreakCircuitPdfGenerator_WhenUsageAtLimit_ReturnsTrue()
+    public void ShouldBreakCircuit_PdfGenerator_When_BelowLimit_Returns_False_And_UpdatesSession()
     {
-        _httpContext.Session.SetInt32(CircuitBreakerOptions.PdfGeneratorKey, 2);
+        _httpContext.Session.SetInt32(CircuitBreakerOptions.PdfGeneratorKey, 1);
         
         bool result = _circuitBreakerService.ShouldBreakCircuit(CircuitBreakerType.PdfGenerator);
-        
-        Assert.That(result, Is.True);
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(result, Is.False);
+            Assert.That(_httpContext.Session.GetInt32(CircuitBreakerOptions.PdfGeneratorKey), Is.EqualTo(2));
+        }
     }
 
     [Test]
-    public void ShouldBreakCircuitPdfGenerator_WhenUsageExceedsLimit_ReturnsTrue()
+    public void ShouldBreakCircuit_PdfGenerator_When_LimitIsReached_Returns_True()
     {
-        _httpContext.Session.SetInt32(CircuitBreakerOptions.PdfGeneratorKey, 3);
+        _httpContext.Session.SetInt32(CircuitBreakerOptions.PdfGeneratorKey, 2);
         
         bool result = _circuitBreakerService.ShouldBreakCircuit(CircuitBreakerType.PdfGenerator);
         
