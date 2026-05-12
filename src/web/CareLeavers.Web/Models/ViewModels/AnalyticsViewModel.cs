5527f713bf450d3560b65915bea65fe0491ef730
Zac-Digital <119302767+Zac-Digital@users.noreply.github.com>
2026-05-12 14:05:22 +0100

    fix: implement GTM as per google docs

--- src/web/CareLeavers.Web/Models/ViewModels/AnalyticsViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CareLeavers.Web.Models.ViewModels;
-
-public sealed class AnalyticsViewModel
-{
-    public string? GoogleAnalyticsTag { get; init; }
-    public string? MicrosoftClarityTag { get; init; }
-    
-    public bool Consent { get; init; }
-}
