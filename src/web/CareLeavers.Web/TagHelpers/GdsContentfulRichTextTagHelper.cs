acaa20bdc7f37742d49bc2a1d2860120f7aabf6d
Zac <119302767+Zac-Digital@users.noreply.github.com>
2026-05-19 16:33:38 +0100

    feat(CL-344): Translation Unavailable Page (#430)
    * refactor: modify translation unavailable page
    
    * fix: spacing issue on phase banner
    
    * feat: record and display time when fresh translations can be done by user
    
    * refactor: use correct layout
    
    * test: update tests for translation unavailable routing
    
    * test: update tests for displaying time

--- src/web/CareLeavers.Web/TagHelpers/GdsContentfulRichTextTagHelper.cs
+++ src/web/CareLeavers.Web/TagHelpers/GdsContentfulRichTextTagHelper.cs
@@ -1,3 +1,4 @@
+using CareLeavers.Web.Configuration;
 using Contentful.AspNetCore.TagHelpers;
 using Contentful.Core.Models;
 using Microsoft.AspNetCore.Razor.TagHelpers;
@@ -5,38 +6,48 @@ using Microsoft.AspNetCore.Razor.TagHelpers;
 namespace CareLeavers.Web.TagHelpers;
 
 [HtmlTargetElement("gds-contentful-rich-text")]
-public class GdsContentfulRichTextTagHelper(HtmlRenderer renderer) : ContentfulRichTextTagHelper(renderer)
+public class GdsContentfulRichTextTagHelper(HtmlRenderer renderer, IHttpContextAccessor accessor)
+    : ContentfulRichTextTagHelper(renderer)
 {
-    public bool LargerText { get; set; } = false;
-    
+    public bool LargerText { get; set; }
+    public bool DynamicTime { get; set; }
+
     public override void Process(TagHelperContext context, TagHelperOutput output)
     {
         output.TagName = null;
         base.Process(context, output);
-        
-        if (LargerText)
-        {
-            var content = output.Content.GetContent();
-            content = content.Replace("govuk-body", "govuk-body-l");
-            output.Content = new DefaultTagHelperContent();
-            output.Content.SetHtmlContent(content);
-        }
-        
+
+        output.Content.SetHtmlContent(ProcessFlags(output.Content.GetContent()));
     }
-    
-    public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+
+    public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
     {
         output.TagName = null;
-        var result = base.ProcessAsync(context, output);
-        result.Wait();
+        await base.ProcessAsync(context, output);
+
+        output.Content.SetHtmlContent(ProcessFlags(output.Content.GetContent()));
+    }
+
+    private string ProcessFlags(string content)
+    {
         if (LargerText)
         {
-            var content = output.Content.GetContent();
             content = content.Replace("govuk-body", "govuk-body-l");
-            output.Content = new DefaultTagHelperContent();
-            output.Content.SetHtmlContent(content);
         }
 
-        return result;
+        if (DynamicTime)
+        {
+            string? timeout =
+                accessor.HttpContext?.Session.GetString(CircuitBreakerOptions.AzureTranslationTimeoutKey);
+
+            content = timeout is not null && content.Contains("((time))")
+                ? content.Replace("((time))", timeout)
+                : """
+                  <p class="govuk-body">You will be able to translate into a new language in 24 hours.</p>
+                  <p class="govuk-body">You can <a class="govuk-link" href="/translate-this-website/home">continue to translate the site</a> into a language you have selected already.</p>
+                  """;
+        }
+
+        return content;
     }
 }
