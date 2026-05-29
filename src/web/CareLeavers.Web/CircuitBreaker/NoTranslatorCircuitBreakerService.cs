2447e848fbaea0caae7782a2c113373aabba472c
Zac-Digital <119302767+Zac-Digital@users.noreply.github.com>
2026-05-29 20:00:04 +0100

    chore: seal class

--- src/web/CareLeavers.Web/CircuitBreaker/NoTranslatorCircuitBreakerService.cs
+++ src/web/CareLeavers.Web/CircuitBreaker/NoTranslatorCircuitBreakerService.cs
@@ -1,6 +1,6 @@
 namespace CareLeavers.Web.CircuitBreaker;
 
-public class NoTranslatorCircuitBreakerService : ITranslatorCircuitBreakerService
+public sealed class NoTranslatorCircuitBreakerService : ITranslatorCircuitBreakerService
 {
     public Task<bool> ShouldOpenCircuit(string html) => Task.FromResult(true);
 }
