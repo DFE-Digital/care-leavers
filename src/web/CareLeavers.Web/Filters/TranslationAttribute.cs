113c8f49b62c83e839d8c5cd4c5e291adfeb0d76
Zac-Digital <119302767+Zac-Digital@users.noreply.github.com>
2026-06-02 09:53:28 +0100

    refactor: specify where attribute can be used

--- src/web/CareLeavers.Web/Filters/TranslationAttribute.cs
+++ src/web/CareLeavers.Web/Filters/TranslationAttribute.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CareLeavers.Web.Filters;
 
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
 public sealed class TranslationAttribute : TypeFilterAttribute
 {
     public TranslationAttribute(bool noCache = false) : base(typeof(TranslationFilter))
