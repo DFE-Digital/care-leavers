62fda692897bd3d5c1f6e863f2dfd91d414afec8
Rachna Mehta <rachna.mehta@education.gov.uk>
2026-07-29 13:56:32 +0100

    feat(CL-602) Using the reporter flag

--- src/e2e/CareLeavers.E2ETests/merge.config.ts
+++
@@ -1,7 +0,0 @@
-// merge.config.ts
-import { defineConfig } from '@playwright/test';
-
-export default defineConfig({
-  // Tell Playwright to use your custom yaml-reporter.ts during the merge
-  reporter: [['./yaml-reporter.ts']],
-});
