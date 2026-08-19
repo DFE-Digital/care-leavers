eea20f61a7a779dfcfcebdcf3571b0a438135d84
Rachna Mehta <rachna.mehta@education.gov.uk>
2026-08-19 14:38:58 +0100

    feat(CL-706): Adding security text

--- src/web/CareLeavers.Web/Configuration/SiteConfiguration.cs
+++ src/web/CareLeavers.Web/Configuration/SiteConfiguration.cs
@@ -6,4 +6,6 @@ namespace CareLeavers.Web.Configuration;
 public static class SiteConfiguration
 {
     public static bool Rebrand { get; set; }
+
+    public static string SecurityTxtUrl { get; set; }
 }
---
+++ src/web/tests/CareLeavers.Web.Tests/Controllers/SecurityTextControllerTests.cs
@@ -0,0 +1,57 @@
+﻿using CareLeavers.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using CareLeavers.Web.Configuration;
+
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CareLeavers.Web.Tests.Controllers
+{
+    public class SecurityTextControllerTests
+    {
+        private SecurityTextController _controller;
+  
+        [SetUp]
+        public void Init()
+        {
+            _controller = new SecurityTextController();
+        }
+        
+        [Test]
+        public void GetSecurityText_NoUrl_Returns404NotFound()
+        {
+            SiteConfiguration.SecurityTxtUrl = string.Empty;
+
+            var result = _controller.GetSecurityText();
+            Assert.That(result, Is.TypeOf<NotFoundObjectResult>());
+                
+            var resultType = result as NotFoundObjectResult;
+            Assert.That(resultType, Is.Not.Null);
+            Assert.That(resultType.StatusCode, Is.EqualTo(404));
+            Assert.That(resultType.Value, Is.EqualTo("Security file was not found"));
+          
+        }
+
+        [Test]
+        public void GetSecurityText_GotUrl_RedirectsToUrl()
+        {
+            SiteConfiguration.SecurityTxtUrl = "https://www.google.com";
+
+            var result = _controller.GetSecurityText();
+            Assert.That(result, Is.TypeOf<RedirectResult>());
+
+            var resultType = result as RedirectResult;
+            Assert.That(resultType, Is.Not.Null);
+            Assert.That(resultType.Url, Is.EqualTo("https://www.google.com"));      
+
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            _controller.Dispose();
+        }
+    }
+}
