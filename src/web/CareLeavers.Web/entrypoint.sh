387df9bae4b2e06a9067d22416564829c2a6d5db
Bob Lawson <armand.lawson@education.gov.uk>
2026-08-21 13:24:07 +0100

    feat(cl-678): pipe collector output to stdout

--- src/web/CareLeavers.Web/entrypoint.sh
+++ src/web/CareLeavers.Web/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Launch Splunk OTEL collector in the background
-/usr/local/bin/otelcol --config=/otel-collector-config.yaml &
+/usr/local/bin/otelcol --config=/otel-collector-config.yaml > /proc/1/fd/1 2>&1 &
 
 # Execute whatever command was passed in (e.g., "dotnet", "CareLeavers.Web.dll")
 exec "$@"
